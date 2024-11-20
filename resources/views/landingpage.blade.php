2eed8e6687f4b8c1681190ba815f122c2ad428d3
netas369 <neve0023@hz.nl>
2024-11-20 15:52:07 +0100

    initial commit

---
+++ resources/views/landingpage.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.main')
+
+@section('title', 'MyAppLietuva')
+
+@section('content')
+    <!-- MAIN CONTENT DIV -->
+    <div class="px-4 sm:px-6 md:px-10 lg:px-20 xl:px-40 2xl:px-60">
+        <div class="text-center mt-10">
+            <h1 class="text-3xl lg:text-4xl font-bold text-primary-light font-franklin leading-7">Rasti profesionalią pagalbą - dabar lengva</h1>
+            <p class="mt-8 text-lg text-primary-light font-arial">Susisiekite tiesiogiai su patikimais paslaugų teikėjais visiems namų poreikiams, nuo žolės
+        pjovimo iki dažymo ir dar daugiau.</p>
+        </div>
+    <div>
+@endsection
+
+@extends('layouts.navbar')
---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+        './resources/views/layouts/*.blade.php',
+    ],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                franklin: ['"Franklin Gothic Heavy"', 'sans-serif'], // Add your font here
+                arial: ['Arial', 'Helvetica', 'sans-serif'], // Add Arial with fallbacks
+            },
+            colors: {
+                primary: {
+                    verylight: '#3ea8a6',
+                    light: '#266867',  // lighter version of primary color
+                    DEFAULT: '#1A4645', // main primary color
+                    dark: '#133232',    // darker version of primary color (optional)
+                },
+            }
+        },
+    },
+
+    plugins: [forms],
+};
