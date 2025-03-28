e60465fe202ecfc26693647c497177332ddfc31c
Netas Neverauskas <neve0023@hz.nl>
2025-03-28 13:16:51 +0100

    become partner page update

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,10 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <title>@yield('title')</title>
     <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;500;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
+{{--    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />--}}
 
     <!-- Include Tailwind compiled by Vite -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @stack('styles')
+
 </head>
 <body class="bg-gray-50 text-gray-800">
 @include('layouts.navbar')
@@ -20,7 +22,9 @@
 @include('layouts.footer')
 
 <!-- Scripts -->
-<script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+{{--<script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>--}}
+@stack('scripts')
+
 @yield('script')
 </body>
 </html>
