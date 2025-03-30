0b71b1446190ad5ff564e0e3d33d8a99775f74de
Netas Neverauskas <neve0023@hz.nl>
2025-03-30 17:35:32 +0200

    lottie test

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -10,13 +10,38 @@
     <!-- Include Tailwind compiled by Vite -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @stack('styles')
-
+    <!-- Custom style to help with iframe positioning -->
+    <style>
+        .lottie-container {
+            width: 100vw;
+            margin-left: calc(-50vw + 50%);
+            max-width: none;
+            overflow: hidden;
+            position: relative;
+        }
+        .lottie-iframe {
+            width: 100%;
+            display: block;
+            margin: 0 auto;
+        }
+    </style>
 </head>
 <body class="bg-gray-50 text-gray-800">
 @include('layouts.navbar')
 
 <div class="container mx-auto px-4">
     @yield('content')
+</div>
+
+<!-- Full-width and centered Lottie animation -->
+<div class="lottie-container">
+    <iframe
+        src="https://cdn.lottielab.com/l/4snxnQuvEVEVwB.html"
+        class="lottie-iframe h-[150px] md:h-[250px] lg:h-[350px]"
+        frameborder="0"
+        title="City skyline animation"
+        loading="lazy"
+    ></iframe>
 </div>
 
 @include('layouts.footer')
