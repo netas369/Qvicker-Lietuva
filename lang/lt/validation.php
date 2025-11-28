d3b5c1a2d7e856f7c19ccef5543e12b0d82f9926
Netas Neverauskas <neve0023@hz.nl>
2025-11-28 16:18:09 +0100

    updated login

---
+++ lang/lt/validation.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    'required' => ':attribute yra privalomas.',
+    'email' => ':attribute turi būti galiojantis el. pašto adresas.',
+    'string' => 'Laukas :attribute turi būti tekstas.',
+    'min' => [
+        'string' => 'Laukas :attribute turi būti bent :min simbolių.',
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Custom Validation Attributes
+    |--------------------------------------------------------------------------
+    */
+    'attributes' => [
+        'email' => 'el. paštas',
+        'password' => 'slaptažodis',
+    ],
+
+];
